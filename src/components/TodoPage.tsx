4d34fb5758b3af3c89104b57ba424df4095c82a7
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-02-02 18:29:08 +0100

    Fetch todos from backend api to TodoPage component

--- src/components/TodoPage.tsx
+++ src/components/TodoPage.tsx
@@ -1,9 +1,34 @@
+import { useState, useEffect } from "react";
+import type { TodoInterface } from "../interfaces/TodoInterface";
+import TodoList from "../components/TodoList";
+import TodoForm from "./TodoForm";
+
 function TodoPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [todos, setTodos] = useState<TodoInterface[]>([]);
+    const [error, setError] = useState<String | null>(null);
+
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+
+    const fetchTodos = async () => {
+        try {
+            const res = await fetch("http://localhost:3000/todos");
+
+            if (res.ok) {
+                const data = await res.json();
+                setTodos(data);
+            }
+        } catch (err) {
+            setError("Gick ej att hämta todos.");
+        }
+    }
+    return (
+        <main>
+            {error && <p>{error}</p>}
+            <TodoList todos={todos}/>
+        </main>
+    )
 }
 
 export default TodoPage;
