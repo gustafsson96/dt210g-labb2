9506100a7951c81802ace30f81024215a8acaa4b
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-02-02 18:47:57 +0100

    Add select box for status of a TodoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -5,15 +5,23 @@ interface Props {
 }
 
 function TodoItem({ todo }: Props) {
-  return (
-    <>
-      <li>
-        <h2>{todo.title}</h2>
-        <p>{todo.description}</p>
-        <p>Status: {todo.status}</p>
-      </li>
-    </>
-  )
+    return (
+        <>
+            <li>
+                <h2>{todo.title}</h2>
+                <p>{todo.description}</p>
+                <p>Status: {todo.status}</p>
+                <form>
+                    <label htmlFor="status">Ändra status:</label>
+                    <select name="status" id="status" defaultValue={todo.status}>
+                        <option value="ej påbörjad">ej påbörjad</option>
+                        <option value="pågående">pågående</option>
+                        <option value="avklarad">avklarad</option>
+                    </select>
+                </form>
+            </li>
+        </>
+    )
 }
 
 
