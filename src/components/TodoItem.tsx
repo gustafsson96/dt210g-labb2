832fff8fa8c3722b81003328ffee962b12abb5ef
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-02-02 19:04:22 +0100

    Add functionality to update a todo status using fetch and create state for error messages

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { TodoInterface } from "../interfaces/TodoInterface";
 
 interface Props {
@@ -5,6 +6,28 @@ interface Props {
 }
 
 function TodoItem({ todo }: Props) {
+    const [error, setError] = useState<string | null>(null);
+
+    const updateStatus = async (e: any) => {
+        const newStatus = e.target.value;
+        const newTodo = {
+            ...todo,
+            status: newStatus
+        }
+        try {
+            const res = await fetch("http://localhost:3000/todos/" + todo.id, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newTodo)
+            });
+            if (!res.ok) throw new Error("Kunde inte uppdatera status.");
+            setError(null);
+        } catch (err) {
+            setError("Kunde inte uppdatera status.");
+        }
+    }
     return (
         <>
             <li>
@@ -13,12 +36,13 @@ function TodoItem({ todo }: Props) {
                 <p>Status: {todo.status}</p>
                 <form>
                     <label htmlFor="status">Ändra status:</label>
-                    <select name="status" id="status" defaultValue={todo.status}>
+                    <select name="status" id={`status-${todo.id}`} defaultValue={todo.status} onChange={updateStatus}>
                         <option value="ej påbörjad">ej påbörjad</option>
                         <option value="pågående">pågående</option>
                         <option value="avklarad">avklarad</option>
                     </select>
                 </form>
+                {error && <p>{error}</p>}
             </li>
         </>
     )
